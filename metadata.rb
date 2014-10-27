7b91636e9e55137d565e52aea3d31ae2812ba50a
Mandeep <mandeep170@users.noreply.github.com>
2014-10-27 15:06:18 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jellyfishopensource@bah.com'
 license          'GPL v2'
 description      'Installs/Configures ManageIQ'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 supports         'rhel'
 
 depends "yum"
