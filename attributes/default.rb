014ff52f2273f59a868f89f5a1bc0e6d63899a15
Chris Kacerguis <kacerguis_christopher@bah.com>
2014-10-31 10:45:13 -0500

    code cleanup

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,7 +8,7 @@ default['manageiq']['db_password']  = "password"
 default['manageiq']['code_repo']    = "https://github.com/booz-allen-hamilton/manageiq"
 
 #RVM setup for miqbuilder
-default['rvm']['user_installs']     = [{'user' => 'miqbuilder' }]
+default['rvm']['user_installs']     = [{'user' => 'miqbuilder'}]
 
 # Automate Code Attributes
 default['manageiq']['automate_code_url']    = "https://s3.amazonaws.com/dpi-releases/BAHdatastore_20141024.zip"
@@ -16,7 +16,7 @@ default['manageiq']['automate_code_domain'] = "BAH"
 
 # PostgreSQL Attributes
 default['postgresql']['password']['postgres']       = node['manageiq']['db_password']
-default["postgresql"]["pg_hba"]                     = [{ type: 'local', db: 'all', user: 'all', addr: '', method: 'trust'}, {type: 'host', db: 'all', user: 'all', addr: '127.0.0.1/32 ', method: 'trust'}]
+default["postgresql"]["pg_hba"]                     = [{type: 'local', db: 'all', user: 'all', addr: '', method: 'trust'}, {type: 'host', db: 'all', user: 'all', addr: '127.0.0.1/32 ', method: 'trust'}]
 default["postgresql"]["config"]["port"]             = 5432
 default["postgresql"]["host"]                       = "127.0.0.1"
 default['postgresql']['config']['listen_addresses'] = "*"
